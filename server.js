24e7c81b89358fbccb08fcf0747f1bba12040ed7
ssigman <ssigman@drury.edu>
2018-11-29 21:27:53 -0600

    Revert "Project converted to Node.js project and web server added."

--- server.js
+++
@@ -1,9 +0,0 @@
-// server.js
-var express = require("express");
-var app = express();
-
-// Serve static files from the public dir
-app.use(express.static("public"));
-
-// Start the web server on port 3000
-app.listen(3000);
